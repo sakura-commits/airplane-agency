55c6ceab067a3fedd4aad0d222049578aa618a72
ABDULKARIM-NASSOR <156984926+ABDULKARIM-NASSOR@users.noreply.github.com>
2026-02-24 10:55:35 +0300

    Implement routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import {Home} from './Pages/Home.jsx';
-import {Contact} from './Pages/Contact.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './Pages/Home.jsx';
+import { Flight } from './Pages/Flight.jsx';
+import { Settings } from './Pages/Settings.jsx';
+import { Profile } from './Pages/Profile.jsx';
+import { MyBookings } from './Pages/MyBookings.jsx';
+import { PaymentMethods } from './Pages/PaymentMethods.jsx';
+import { TravelPreferences } from './Pages/TravelPreferences.jsx';
 
 function App() {
-  
-
   return (
-    <>
-    <Contact/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/flights" element={<Flight />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/payment-methods" element={<PaymentMethods />} />
+        <Route path="/preferences" element={<TravelPreferences />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
